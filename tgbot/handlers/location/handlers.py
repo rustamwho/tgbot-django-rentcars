1286f715194f9c23d2d63ce65f424ccd7e631334
rustamwho <40338874+rustamwho@users.noreply.github.com>
2021-10-04 14:57:32 +0300

    Initial commit

---
+++ tgbot/handlers/location/handlers.py
@@ -0,0 +1,29 @@
+import telegram
+from telegram import Update
+
+from tgbot.handlers.location.static_text import share_location, thanks_for_location
+from tgbot.handlers.location.keyboard_utils import send_location_keyboard
+from tgbot.models import User, Location
+
+
+def ask_for_location(update: Update, context) -> None:
+    """ Entered /ask_location command"""
+    u = User.get_user(update, context)
+
+    context.bot.send_message(
+        chat_id=u.user_id,
+        text=share_location,
+        reply_markup=send_location_keyboard()
+    )
+
+
+def location_handler(update: Update, context) -> None:
+    # receiving user's location
+    u = User.get_user(update, context)
+    lat, lon = update.message.location.latitude, update.message.location.longitude
+    Location.objects.create(user=u, latitude=lat, longitude=lon)
+
+    update.message.reply_text(
+        thanks_for_location,
+        reply_markup=telegram.ReplyKeyboardRemove(),
+    )
