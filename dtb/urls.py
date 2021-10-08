a8091af130ee5f232cef3a31d51c647caebfdfde
rustamwho <rustamwho@gmail.com>
2021-10-08 20:05:31 +0300

    Urls for media when DEBUG

--- dtb/urls.py
+++ dtb/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', include('tgbot.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
