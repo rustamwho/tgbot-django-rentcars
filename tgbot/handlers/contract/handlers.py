c8bcd52525bb046f76028a77f5ddfe28225aaf84
rustamwho <rustamwho@gmail.com>
2021-10-09 21:11:43 +0300

    Start Conversation Handler for Personal Data

--- tgbot/handlers/contract/handlers.py
+++ tgbot/handlers/contract/handlers.py
@@ -1,21 +1,56 @@
 import datetime
 import io
 
-from telegram import ParseMode, Update
+from telegram import ParseMode, Update, ReplyKeyboardRemove
 from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext import MessageHandler, ConversationHandler, Filters, \
+    CommandHandler, CallbackQueryHandler
 from django.core.files import File
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
 
 from general_utils.utils import get_verbose_date
+from general_utils.constants import GENDER_CHOICES
 from tgbot.models import User
+from tgbot.handlers.contract import static_text, keyboard_utils
 
 from rentcars.utils.contracts import create_contract
 from rentcars.models import Contract
+from rentcars import validators
+
+(FIRST_NAME, LAST_NAME, MIDDLE_NAME, GENDER, BIRTHDAY, EMAIL, PHONE_NUMBER,
+ PASSPORT_SERIAL, PASSPORT_NUMBER, PASSPORT_ISSUED_AT, PASSPORT_ISSUED_BY,
+ ADDRESS_REGISTRATION, ADDRESS_RESIDENCE, CLOSE_PERSON_NAME,
+ CLOSE_PERSON_PHONE, CLOSE_PERSON_ADDRESS) = range(16)
 
 
 def start_contract(update: Update, context: CallbackContext) -> None:
     u = User.get_user(update, context)
 
-    new_contract = create_contract(u)
+    if u.contract.exists():
+        valid_contracts = u.contract.filter(closed_at__gte=now().date())
+        if valid_contracts.exists():
+            valid_contract = valid_contracts.order_by('closed_at').last()
+            days = (valid_contract.closed_at - now().date()).days
+            update.message.reply_text(f'До конца действия договора осталось '
+                                      f'{days} дней.')
+            return ConversationHandler.END
+
+    context.bot.send_message(
+        chat_id=u.user_id,
+        text=static_text.NOT_EXISTS_CONTRACTS
+    )
+    context.bot.send_message(
+        chat_id=u.user_id,
+        text=static_text.ABOUT_FILLING_PERSONAL_DATA
+    )
+
+    update.message.reply_text(
+        text=static_text.ASK_FIRST_NAME,
+        parse_mode=ParseMode.HTML
+    )
+    return FIRST_NAME
+    """new_contract = create_contract(u)
     new_contract_io = io.BytesIO()
     new_contract.save(new_contract_io)
     new_contract_io.seek(0)
@@ -30,4 +65,136 @@ def start_contract(update: Update, context: CallbackContext) -> None:
         document=contr.file,
         filename=(u.last_name + ' ' + u.first_name + ' ' +
                   get_verbose_date(contr.created_at) + '.docx')
+    )"""
+
+
+def first_name_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.russian_letters_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return FIRST_NAME
+
+    context.user_data[FIRST_NAME] = text
+    update.message.reply_text(
+        text=static_text.ASK_LAST_NAME,
+        parse_mode=ParseMode.HTML
     )
+    return LAST_NAME
+
+
+def last_name_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.russian_letters_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return LAST_NAME
+
+    context.user_data[LAST_NAME] = text
+    update.message.reply_text(
+        text=static_text.ASK_MIDDLE_NAME,
+        parse_mode=ParseMode.HTML
+    )
+    return MIDDLE_NAME
+
+
+def middle_name_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.russian_letters_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return MIDDLE_NAME
+
+    context.user_data[MIDDLE_NAME] = text
+
+    u = User.get_user(update, context)
+    name = (f'{context.user_data[LAST_NAME]} {context.user_data[FIRST_NAME]} '
+            f'{context.user_data[MIDDLE_NAME]}')
+    context.bot.send_message(
+        chat_id=u.user_id,
+        text=static_text.HELLO_FULL_NAME.format(name=name)
+    )
+
+    update.message.reply_text(
+        text=static_text.ASK_GENDER,
+        parse_mode=ParseMode.HTML,
+        reply_markup=keyboard_utils.get_keyboard_for_gender()
+    )
+
+    return GENDER
+
+
+def gender_handler(update: Update, context: CallbackContext) -> int:
+    gender = int(update.callback_query.data)
+    context.user_data[GENDER] = gender
+
+    update.effective_message.edit_text(f'Ваш пол: {GENDER_CHOICES[gender][1]}')
+
+    update.effective_message.reply_text(
+        text=static_text.ASK_BIRTHDAY,
+        parse_mode=ParseMode.HTML,
+    )
+    return BIRTHDAY
+
+
+def birthday_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.birthday_date_validate(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return BIRTHDAY
+
+    context.user_data[BIRTHDAY] = text
+
+    update.message.reply_text('Спасибо, но еще не все, скоро меня доработают.')
+
+    return ConversationHandler.END
+
+
+def cancel_handler(update: Update, context: CallbackContext):
+    """ Отменить весь процесс диалога. Данные будут утеряны
+    """
+    update.message.reply_text('Отмена. Для начала с нуля нажмите /start')
+    return ConversationHandler.END
+
+
+def get_conversation_handler_for_contract():
+    conv_handler = ConversationHandler(
+        entry_points=[
+            CommandHandler('contract', start_contract),
+        ],
+        states={
+            FIRST_NAME: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               first_name_handler, pass_user_data=True)
+            ],
+            LAST_NAME: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               last_name_handler, pass_user_data=True)
+            ],
+            MIDDLE_NAME: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               middle_name_handler, pass_user_data=True)
+            ],
+            GENDER: [
+                CallbackQueryHandler(gender_handler, pass_user_data=True)
+            ],
+            BIRTHDAY: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               birthday_handler, pass_user_data=True)
+            ]
+
+        },
+        fallbacks=[
+            CommandHandler('cancel', cancel_handler),
+        ]
+    )
+    return conv_handler
