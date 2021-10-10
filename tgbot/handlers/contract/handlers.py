5bdfd8d143349e4425ef6093405dfb613ca0d0a0
rustamwho <rustamwho@gmail.com>
2021-10-10 11:18:36 +0300

    Add handlers

--- tgbot/handlers/contract/handlers.py
+++ tgbot/handlers/contract/handlers.py
@@ -7,6 +7,7 @@ from telegram.ext import MessageHandler, ConversationHandler, Filters, \
     CommandHandler, CallbackQueryHandler
 from django.core.files import File
 from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 from django.utils.timezone import now
 
 from general_utils.utils import get_verbose_date
@@ -154,7 +155,287 @@ def birthday_handler(update: Update, context: CallbackContext) -> int:
 
     context.user_data[BIRTHDAY] = text
 
-    update.message.reply_text('Спасибо, но еще не все, скоро меня доработают.')
+    update.message.reply_text(
+        text=static_text.ASK_EMAIL,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return EMAIL
+
+
+def email_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validator = EmailValidator(
+            message='Адрес электронной почты должен быть правильным. '
+                    'Например, rustamwho@mail.com')
+        validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return EMAIL
+
+    context.user_data[EMAIL] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_PHONE_NUMBER,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return PHONE_NUMBER
+
+
+def phone_number_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.phone_number_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return PHONE_NUMBER
+
+    context.user_data[PHONE_NUMBER] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_PASSPORT_SERIAL,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return PASSPORT_SERIAL
+
+
+def passport_serial_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.passport_serial_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return PASSPORT_SERIAL
+
+    context.user_data[PASSPORT_SERIAL] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_PASSPORT_NUMBER,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return PASSPORT_NUMBER
+
+
+def passport_number_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.passport_number_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return PASSPORT_NUMBER
+
+    context.user_data[PASSPORT_NUMBER] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_PASSPORT_DATE_OF_ISSUE,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return PASSPORT_ISSUED_AT
+
+
+def passport_issued_at_handler(update: Update,
+                               context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.date_validate(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return PASSPORT_ISSUED_AT
+
+    context.user_data[PASSPORT_ISSUED_AT] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_PASSPORT_ISSUED_BY,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return PASSPORT_ISSUED_BY
+
+
+def passport_issued_by_handler(update: Update,
+                               context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.passport_issued_by_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return PASSPORT_ISSUED_BY
+
+    context.user_data[PASSPORT_ISSUED_BY] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_ADDRESS_REGISTRATION,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return ADDRESS_REGISTRATION
+
+
+def address_registration_handler(update: Update,
+                                 context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.address_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return ADDRESS_REGISTRATION
+
+    context.user_data[ADDRESS_REGISTRATION] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_ADDRESS_RESIDENCE_FIRST,
+        parse_mode=ParseMode.HTML,
+        reply_markup=keyboard_utils.get_keyboard_for_address_similar(),
+    )
+
+    return ADDRESS_RESIDENCE
+
+
+def address_residence_similar_handler(update: Update,
+                                      context: CallbackContext) -> int:
+    answer = update.callback_query.data
+    if answer == 'similar_addr':
+        address = context.user_data[ADDRESS_REGISTRATION]
+        context.user_data[ADDRESS_RESIDENCE] = address
+
+        update.effective_message.edit_text('Адрес проживания совпадает с '
+                                           'адресом прописки.')
+
+        update.effective_message.reply_text(
+            text=static_text.ASK_CLOSE_PERSON_NAME,
+            parse_mode=ParseMode.HTML,
+        )
+
+        return CLOSE_PERSON_NAME
+
+    update.effective_message.edit_text(
+        text=static_text.ASK_ADDRESS_RESIDENCE_SECOND,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return ADDRESS_RESIDENCE
+
+
+def address_residence_diff_handler(update: Update,
+                                   context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.address_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return ADDRESS_RESIDENCE
+
+    context.user_data[ADDRESS_RESIDENCE] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_CLOSE_PERSON_NAME,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return CLOSE_PERSON_NAME
+
+
+def close_person_name_handler(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.close_person_name_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return CLOSE_PERSON_NAME
+
+    context.user_data[CLOSE_PERSON_NAME] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_CLOSE_PERSON_PHONE.format(close_person_name=text),
+        parse_mode=ParseMode.HTML
+    )
+
+    return CLOSE_PERSON_PHONE
+
+
+def close_person_phone_handler(update: Update,
+                               context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.phone_number_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return CLOSE_PERSON_PHONE
+
+    context.user_data[CLOSE_PERSON_PHONE] = text
+
+    update.message.reply_text(
+        text=static_text.ASK_CLOSE_PERSON_ADDRESS_FIRST,
+        parse_mode=ParseMode.HTML,
+        reply_markup=keyboard_utils.get_keyboard_for_address_similar(),
+    )
+
+    return CLOSE_PERSON_ADDRESS
+
+
+def close_person_address_similar_handler(update: Update,
+                                         context: CallbackContext) -> int:
+    answer = update.callback_query.data
+    if answer == 'similar_addr':
+        address = context.user_data[ADDRESS_RESIDENCE]
+        context.user_data[CLOSE_PERSON_ADDRESS] = address
+
+        update.effective_message.edit_text(
+            'Адрес проживания близкого человека совпадает с местом проживания '
+            'арендатора.'
+        )
+
+        update.effective_message.reply_text(
+            text=static_text.END_PERSONAL_DATA,
+        )
+
+        print(context.user_data)
+
+        return ConversationHandler.END
+
+    update.effective_message.edit_text(
+        text=static_text.ASK_CLOSE_PERSON_ADDRESS_SECOND,
+        parse_mode=ParseMode.HTML,
+    )
+
+    return CLOSE_PERSON_ADDRESS
+
+
+def close_person_address_diff_handler(update: Update,
+                                      context: CallbackContext) -> int:
+    text = update.message.text
+
+    try:
+        validators.address_validator(text)
+    except ValidationError as e:
+        update.message.reply_text(e.message + '\n\nПовторите ввод.')
+        return CLOSE_PERSON_ADDRESS
+
+    context.user_data[CLOSE_PERSON_ADDRESS] = text
+
+    update.message.reply_text(
+        text=static_text.END_PERSONAL_DATA,
+    )
+
+    print(context.user_data)
+    # TODO: Написать красивый вывод юзеру
+    # TODO: Расписать докстринги для функций
+    # TODO: Написать сохранение в базу
 
     return ConversationHandler.END
 
@@ -162,7 +443,7 @@ def birthday_handler(update: Update, context: CallbackContext) -> int:
 def cancel_handler(update: Update, context: CallbackContext):
     """ Отменить весь процесс диалога. Данные будут утеряны
     """
-    update.message.reply_text('Отмена. Для начала с нуля нажмите /start')
+    update.message.reply_text('Отмена. Для начала с нуля нажмите /contract')
     return ConversationHandler.END
 
 
@@ -190,8 +471,58 @@ def get_conversation_handler_for_contract():
             BIRTHDAY: [
                 MessageHandler(Filters.text & ~Filters.command,
                                birthday_handler, pass_user_data=True)
-            ]
-
+            ],
+            EMAIL: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               email_handler, pass_user_data=True)
+            ],
+            PHONE_NUMBER: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               phone_number_handler, pass_user_data=True)
+            ],
+            PASSPORT_SERIAL: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               passport_serial_handler, pass_user_data=True)
+            ],
+            PASSPORT_NUMBER: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               passport_number_handler, pass_user_data=True)
+            ],
+            PASSPORT_ISSUED_AT: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               passport_issued_at_handler, pass_user_data=True)
+            ],
+            PASSPORT_ISSUED_BY: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               passport_issued_by_handler, pass_user_data=True)
+            ],
+            ADDRESS_REGISTRATION: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               address_registration_handler,
+                               pass_user_data=True)
+            ],
+            ADDRESS_RESIDENCE: [
+                CallbackQueryHandler(address_residence_similar_handler,
+                                     pass_user_data=True),
+                MessageHandler(Filters.text & ~Filters.command,
+                               address_residence_diff_handler,
+                               pass_user_data=True)
+            ],
+            CLOSE_PERSON_NAME: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               close_person_name_handler, pass_user_data=True)
+            ],
+            CLOSE_PERSON_PHONE: [
+                MessageHandler(Filters.text & ~Filters.command,
+                               close_person_phone_handler, pass_user_data=True)
+            ],
+            CLOSE_PERSON_ADDRESS: [
+                CallbackQueryHandler(close_person_address_similar_handler,
+                                     pass_user_data=True),
+                MessageHandler(Filters.text & ~Filters.command,
+                               close_person_address_diff_handler,
+                               pass_user_data=True)
+            ],
         },
         fallbacks=[
             CommandHandler('cancel', cancel_handler),
