97d0a00afead80329c96b2eeda1fcd8344d1ba62
rustamwho <rustamwho@gmail.com>
2021-10-12 16:21:43 +0300

    - Add hello text for /start

--- tgbot/handlers/onboarding/static_text.py
+++ tgbot/handlers/onboarding/static_text.py
@@ -1,5 +1,9 @@
-start_created = "Sup, {first_name}!"
-start_not_created = "Welcome back, {first_name}!"
+start_created = "Здравствуйте, {first_name}!"
+start_not_created = "С возвращением, {first_name}!"
+ANSWER_TO_UNKNOWN = ('Нажмите на значок меню внизу, рядом со скрепкой, '
+                     'чтобы увидеть команды.\n\n'
+                     'Вот тут\n'
+                     '   ↓\n')
 unlock_secret_room = "Congratulations! You've opened a secret room👁‍🗨. There is some information for you:\n" \
            "<b>Users</b>: {user_count}\n" \
            "<b>24h active</b>: {active_24}"
