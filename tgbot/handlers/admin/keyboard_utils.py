4b10a51953f0158fcec9a01b4d366648fbd81e79
rustamwho <rustamwho@gmail.com>
2021-10-15 20:42:28 +0300

    Update keyboard of admin menu

--- tgbot/handlers/admin/keyboard_utils.py
+++ tgbot/handlers/admin/keyboard_utils.py
@@ -3,10 +3,18 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from tgbot.handlers.admin import manage_data
 
 
-def get_admin_main_keyboard():
-    buttons = [[
-        InlineKeyboardButton('Получить список всех пользователей',
-                             callback_data=manage_data.GET_ALL_USERS)
-    ]]
+def get_admin_main_menu_keyboard():
+    buttons = [
+        [
+            InlineKeyboardButton('Все пользователи',
+                                 callback_data=manage_data.GET_ALL_USERS),
+            InlineKeyboardButton('Арендаторы',
+                                 callback_data=manage_data.GET_ARENDATORS)
+        ],
+        [
+            InlineKeyboardButton('Закрыть ✅',
+                                 callback_data=manage_data.REMOVE_KEYBOARD)
+        ]
+    ]
 
     return InlineKeyboardMarkup(buttons)
