d69f109e83d7b8606ad37981432644fd2a1d7731
rustamwho <rustamwho@gmail.com>
2021-10-15 19:32:42 +0300

    Function for receiving all users

--- tgbot/handlers/admin/utils.py
+++ tgbot/handlers/admin/utils.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from django.db.models import QuerySet
 from typing import Dict
 
+from tgbot.models import User
+
 
 def _get_csv_from_qs_values(queryset: QuerySet[Dict], filename: str = 'users'):
     keys = queryset[0].keys()
@@ -28,3 +30,47 @@ def _get_csv_from_qs_values(queryset: QuerySet[Dict], filename: str = 'users'):
     buf.name = f"{filename}__{datetime.now().strftime('%Y.%m.%d.%H.%M')}.csv"
 
     return buf
+
+
+def get_text_all_users():
+    """Return text with ALL users."""
+    users_list = User.objects.all()
+
+    text = ''
+    users_without_pd = []
+    i = 1
+
+    for u in users_list:
+        if hasattr(u, 'personal_data'):
+            user_t = (u.personal_data.last_name + ' ' +
+                      u.personal_data.first_name + ' ' +
+                      u.personal_data.middle_name)
+            text += f'{i}. {user_t}. @{u.username}\n'
+            i += 1
+        else:
+            users_without_pd.append(u)
+
+    if not users_without_pd:
+        return text
+
+    text += '\nПользователи без персональных данных:\n'
+
+    users_without_username = []
+
+    for u in users_without_pd:
+        if u.username is not None:
+            text += f'{i}. @{u.username}\n'
+        else:
+            users_without_username.append(u)
+        i += 1
+
+    if not users_without_username:
+        return text
+
+    text += '\nПользователи без никнейма в телеграме:\n'
+
+    for u in users_without_username:
+        text += f'{i}. Telegram ID - {u.user_id}'
+        i += 1
+
+    return text
