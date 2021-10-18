682e47d58fc1a218c0d7f262a58b2f0d92687bf2
rustamwho <rustamwho@gmail.com>
2021-10-18 19:39:01 +0300

    Keyboard for approving contract

--- tgbot/handlers/admin/keyboard_utils.py
+++ tgbot/handlers/admin/keyboard_utils.py
@@ -12,9 +12,28 @@ def get_admin_main_menu_keyboard():
                                  callback_data=manage_data.GET_ARENDATORS)
         ],
         [
+            InlineKeyboardButton(
+                'Неподтвержденные договоры',
+                callback_data=manage_data.GET_UNAPPROVED_CONTRACTS)
+        ],
+        [
             InlineKeyboardButton('Закрыть ✅',
                                  callback_data=manage_data.REMOVE_KEYBOARD)
         ]
     ]
+
+    return InlineKeyboardMarkup(buttons)
+
+
+def get_approve_contract_keyboard(contract_id: int):
+    buttons = [[
+        InlineKeyboardButton(
+            'Подтвердить ✅',
+            callback_data=(
+                    manage_data.BASE_FOR_APPROVE_CONTRACT + str(contract_id))),
+        InlineKeyboardButton('Потом',
+                             callback_data=manage_data.DELETE_MESSAGE)
+
+    ]]
 
     return InlineKeyboardMarkup(buttons)
