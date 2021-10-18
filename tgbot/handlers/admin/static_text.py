43994ba3a1dce158887efb90201b5e3d9844ed80
rustamwho <rustamwho@gmail.com>
2021-10-18 19:38:16 +0300

    Static text for approving contract

--- tgbot/handlers/admin/static_text.py
+++ tgbot/handlers/admin/static_text.py
@@ -5,3 +5,10 @@ ADMIN_MENU_BASE = f'⚠️ Выберите нужное действие\n{comm
 
 users_amount_stat = "<b>Users</b>: {user_count}\n" \
                     "<b>24h active</b>: {active_24}"
+
+NOW_SEND_UNAPPROVED_CONTRACTS = 'Сейчас скину неподтвержденные договоры.'
+
+TEXT_FOR_APPROVE_CONTRACT = ('<b>Договор от {created_at}</b> \n'
+                             '<b>Арендатор:</b> {name_arendator}\n'
+                             '<b>Действителен до:</b> {closed_at}')
+CONTRACT_IS_APPROVED = '\n\n✅ Подтвержден ✅'
