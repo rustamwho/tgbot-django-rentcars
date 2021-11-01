382b9d2dd2037220ff5c281613849ec4d8ae9dae
rustamwho <rustamwho@gmail.com>
2021-11-01 19:08:15 +0300

    Update approve keyboard and add keyboard for setting car to contract

--- tgbot/handlers/admin/keyboard_utils.py
+++ tgbot/handlers/admin/keyboard_utils.py
@@ -1,5 +1,6 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+from rentcars.models import Car
 from tgbot.handlers.admin import manage_data
 
 
@@ -29,16 +30,67 @@ def get_admin_main_menu_keyboard():
     return InlineKeyboardMarkup(buttons)
 
 
-def get_approve_contract_keyboard(contract_id: int):
-    buttons = [[
-        InlineKeyboardButton(
-            'Подтвердить ✅',
-            callback_data=(
-                    manage_data.BASE_FOR_APPROVE_CONTRACT + str(contract_id))),
-        InlineKeyboardButton('Потом',
-                             callback_data=manage_data.DELETE_MESSAGE)
+def get_set_car_to_contract_keyboard(contract_id: int, free_cars: list[Car]):
+    """
+    Get keyboard with buttons with
+    callback data = BASE_FOR_SET_CAR_TO_CONTRACT_contractId_carId
+    """
+    buttons = [
+        [
+            InlineKeyboardButton(
+                f'🚘 {car.license_plate} {car.model}',
+                callback_data=(
+                        manage_data.BASE_FOR_SET_CAR_TO_CONTRACT +
+                        str(contract_id) + '_' + str(car.id)
+                )
+            )
+        ]
+        for car in free_cars
+    ]
 
-    ]]
+    buttons.append(
+        [InlineKeyboardButton('Потом ↩️',
+                              callback_data=manage_data.DELETE_MESSAGE)]
+    )
+
+    return InlineKeyboardMarkup(buttons)
+
+
+def get_approve_contract_keyboard(contract_id: int,
+                                  is_car_exists: bool = True):
+    """
+    If car of contract does not exists - return button for menu setting car
+    Else return button for approving contract
+    """
+    if is_car_exists is True:
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    'Подтвердить ✅',
+                    callback_data=(
+                            manage_data.BASE_FOR_APPROVE_CONTRACT +
+                            str(contract_id))),
+                InlineKeyboardButton(
+                    'Изменить 🚘',
+                    callback_data=(
+                            manage_data.SET_CAR_TO_CONTRACT_MENU +
+                            str(contract_id))),
+            ],
+            [
+                InlineKeyboardButton('Потом ↩️',
+                                     callback_data=manage_data.DELETE_MESSAGE)
+            ]
+        ]
+    else:
+        buttons = [[
+            InlineKeyboardButton(
+                'Назначить 🚘',
+                callback_data=(
+                        manage_data.SET_CAR_TO_CONTRACT_MENU +
+                        str(contract_id))),
+            InlineKeyboardButton('Потом ↩️',
+                                 callback_data=manage_data.DELETE_MESSAGE)
+        ]]
 
     return InlineKeyboardMarkup(buttons)
 
