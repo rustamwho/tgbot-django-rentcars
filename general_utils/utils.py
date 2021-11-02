04290d4202025835681586b5b3aad2383901ef5e
rustamwho <rustamwho@gmail.com>
2021-11-02 18:19:37 +0300

    Transliterate license plate

--- general_utils/utils.py
+++ general_utils/utils.py
@@ -1,6 +1,18 @@
 import datetime
 
+from general_utils.constants import trans_dict_eng_rus
+
 
 def get_verbose_date(date: datetime.date) -> str:
-    """Return date in string 'dd.mm.yyyy' format"""
+    """Return date in string 'dd.mm.yyyy' format."""
     return datetime.date.strftime(date, '%d.%m.%Y')
+
+
+def transliterate_license_plate(license_plate: str) -> str:
+    """Return License plate in russian language."""
+    for symbol in license_plate:
+        if symbol.isalpha():
+            print('ras')
+            license_plate = license_plate.replace(symbol,
+                                                  trans_dict_eng_rus[symbol])
+    return license_plate
