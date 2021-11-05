02fd9b79ce2a85814089445070d7ca19075dada3
rustamwho <rustamwho@gmail.com>
2021-11-05 23:24:29 +0300

    Fix admin site header and unregister groups

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -11,6 +11,9 @@ from tgbot.tasks import broadcast_message
 from tgbot.handlers.broadcast_message.utils import _send_message
 
 from rentcars.models import PersonalData
+
+admin.site.site_header = 'Администрирование Elit Transfer'
+admin.site.unregister(auth.models.Group)
 
 
 class QuestionInline(admin.StackedInline):
@@ -34,7 +37,8 @@ class UserAdmin(admin.ModelAdmin):
 
     def broadcast(self, request, queryset):
         """ Select users via check mark in django-admin panel, then select "Broadcast" to send message"""
-        user_ids = queryset.values_list('user_id', flat=True).distinct().iterator()
+        user_ids = queryset.values_list('user_id',
+                                        flat=True).distinct().iterator()
         if 'apply' in request.POST:
             broadcast_message_text = request.POST["broadcast_text"]
 
@@ -44,14 +48,18 @@ class UserAdmin(admin.ModelAdmin):
                         user_id=user_id,
                         text=broadcast_message_text,
                     )
-                self.message_user(request, f"Just broadcasted to {len(queryset)} users")
+                self.message_user(request,
+                                  f"Just broadcasted to {len(queryset)} users")
             else:
-                broadcast_message.delay(text=broadcast_message_text, user_ids=list(user_ids))
-                self.message_user(request, f"Broadcasting of {len(queryset)} messages has been started")
+                broadcast_message.delay(text=broadcast_message_text,
+                                        user_ids=list(user_ids))
+                self.message_user(request,
+                                  f"Broadcasting of {len(queryset)} messages has been started")
 
             return HttpResponseRedirect(request.get_full_path())
         else:
             form = BroadcastForm(initial={'_selected_action': user_ids})
             return render(
-                request, "admin/broadcast_message.html", {'form': form, 'title': u'Broadcast message'}
+                request, "admin/broadcast_message.html",
+                {'form': form, 'title': u'Broadcast message'}
             )
