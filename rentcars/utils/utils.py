102a98a2ee04f2e24cf3811afdbc478343bd485e
rustamwho <rustamwho@gmail.com>
2021-11-06 18:34:04 +0300

    Moved transliterate of license plate

---
+++ rentcars/utils/utils.py
@@ -0,0 +1,11 @@
+from rentcars.utils.constants import trans_dict_eng_rus
+
+
+def transliterate_license_plate(license_plate: str) -> str:
+    """Return License plate in russian language."""
+    for symbol in license_plate:
+        if symbol.isalpha():
+            print('ras')
+            license_plate = license_plate.replace(symbol,
+                                                  trans_dict_eng_rus[symbol])
+    return license_plate
