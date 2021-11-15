802971f5bb468a1546bdf2397eb953a7ffe26935
rustamwho <rustamwho@gmail.com>
2021-11-15 20:09:41 +0300

    Small fix

--- rentcars/admin.py
+++ rentcars/admin.py
@@ -12,7 +12,7 @@ class PhotoCarContractInline(admin.TabularInline):
 @admin.register(PersonalData)
 class PersonalDataAdmin(admin.ModelAdmin):
     list_display = (
-        'user', 'first_name', 'last_name', 'middle_name', 'gender', 'email',
+        'last_name', 'first_name', 'middle_name', 'gender', 'email',
         'birthday', 'phone_number', 'passport_serial', 'passport_number',
         'passport_date_of_issue', 'passport_issued_by', 'address_registration',
         'close_person_name', 'created_at', 'updated_at')
