a89495c3095409da69aecbb46b4d4fe23be99cb1
rustamwho <rustamwho@gmail.com>
2021-11-15 20:19:47 +0300

    Path for screenshots of fine

--- rentcars/utils/paths.py
+++ rentcars/utils/paths.py
@@ -6,3 +6,9 @@ def car_photos_path(instance, filename):
 def contract_photos_path(instance, filename):
     user_id = instance.contract.user.user_id
     return 'contracts/photo_car/user_{0}/{1}'.format(user_id, filename)
+
+
+def fine_screens_path(instance, filename=None):
+    filename = (f'{instance.car.license_plate[:-3]} '
+                f'{instance.get_date_in_str()}_{instance.amount}rub')
+    return f'fines/{filename}.jpg'
