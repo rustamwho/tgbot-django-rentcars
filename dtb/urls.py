31fc1a27cc66660e0d4871fa0b01e1ef00218658
rustamwho <rustamwho@gmail.com>
2021-11-24 12:35:23 +0300

    Delete debug toolbar

--- dtb/urls.py
+++ dtb/urls.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('tgadmin/', admin.site.urls),
     path('', include('tgbot.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:
