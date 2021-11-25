bc329d63d4ed733cb6b96aa9015ec7251c1953ef
rustamwho <rustamwho@gmail.com>
2021-11-25 14:34:01 +0300

    Delete app.task

--- tgbot/dispatcher.py
+++ tgbot/dispatcher.py
@@ -120,7 +120,7 @@ except telegram.error.Unauthorized:
     sys.exit(1)
 
 
-@app.task(ignore_result=True)
+# @app.task(ignore_result=True)
 def process_telegram_event(update_json):
     update = Update.de_json(update_json, bot)
     dispatcher.process_update(update)
